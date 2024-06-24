a662f146fbfeac485c3d5601f1233db987ef75ad
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-06-24 22:52:06 +0200

    change frontend route

--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -5,25 +5,35 @@ import { GN2CommonModule } from '@geonature_common/GN2Common.module';
 import { OrganismListMapComponent } from './organism-list-map/organism-list-map.component';
 import { OrganismMapFormComponent } from './organism-map-form/organism-map-form.component';
 import { OrganismDetailsComponent } from './organism-details/organism-details.component';
+import { OrganismResolver } from './resolvers/organism.resolver';
+import { OrganismStoreService } from './data.service';
+import { ModalDeleteOrganism } from './delete-modal/delete-modal.component';
 
 // my module routing
 const routes: Routes = [
-    { path: '', component: OrganismListMapComponent },
-    { path: 'edit', component: OrganismMapFormComponent },
-    { path: 'create', component: OrganismMapFormComponent},
-    { path: 'info', component: OrganismDetailsComponent}
+  { path: '', component: OrganismListMapComponent },
+  {
+    path: 'edit/:id_organism',
+    component: OrganismMapFormComponent,
+    resolve: { organism: OrganismResolver },
+  },
+  { path: 'create', component: OrganismMapFormComponent },
+  {
+    path: 'info/:id_organism',
+    component: OrganismDetailsComponent,
+    resolve: { organism: OrganismResolver },
+  },
 ];
 
 @NgModule({
-    declarations: [
-        OrganismDetailsComponent,OrganismMapFormComponent,OrganismListMapComponent
-    ],
-    imports: [
-        GN2CommonModule,
-        CommonModule,
-        RouterModule.forChild(routes),
-    ],
-    providers: [],
-    bootstrap: [],
+  declarations: [
+    OrganismDetailsComponent,
+    OrganismMapFormComponent,
+    OrganismListMapComponent,
+    ModalDeleteOrganism,
+  ],
+  imports: [GN2CommonModule, CommonModule, RouterModule.forChild(routes)],
+  providers: [OrganismResolver, OrganismStoreService],
+  bootstrap: [],
 })
-export class GeonatureModule { }
+export class GeonatureModule {}
